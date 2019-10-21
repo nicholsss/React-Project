5270e0c4bd1acddda8abb7397db6510ab9c5607d
Nicholsss <mickyaw1995@gmail.com>
2019-10-21 17:42:03 +0300

    Router made

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react'
-import RegistrationForm from './components/RegistrationForm'
+import {
+  BrowserRouter as Router,
+  Route, Link, Redirect, withRouter
+} from 'react-router-dom'
 
+import RegistrationForm from './components/RegistrationForm'
+import LoginForm from './components/LoginForm'
 
 const App = () => {
+  const padding = { padding: 5 }
   return (
     <div className="App">
+      <Router>
+        <div>
+        <Link style={padding} to="/">Home</Link>
+      <Link style={padding} to="/login">Login</Link>
+      <Link style={padding} to="/register">Register</Link>
+      </div>
+      <div>
+      <Route exact path="/login" render={() => <LoginForm />} />
+      <Route exact path="/register" render={() => <RegistrationForm />} />
+      </div>
+      </Router>
       
-      {RegistrationForm()}
+      
     </div>
   );
 }
