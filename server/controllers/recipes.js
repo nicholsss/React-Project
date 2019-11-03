8cedec2996839c3d81ce4c3f003ca09cc45b6cec
Nicholsss <mickyaw1995@gmail.com>
2019-11-03 20:24:25 +0200

    recipe can be added to backend

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -8,7 +8,45 @@ recipesRouter.get("/", async (request, response) => {
   response.json(recipes.map(r => r.toJSON()));
 });
 
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 recipesRouter.post("/", async (request, response) => {
+
+  const body = request.body
+
+  const token = getTokenFrom(request)
+  
+  try {
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+
+    const user = await User.findById(decodedToken.id)
+
+    const recipe = new Recipe({
+      title: body.title,
+      category: body.category,
+      ingredient:body.ingredients,
+      instruction:body.instruction,
+      user: user._id
+    })
+
+    const savedRecipe = await recipe.save()
+    user.recipes = user.recipes.concat(savedRecipe._id)
+    await user.save()
+    response.json(savedRecipe.toJSON())
+  } catch(exception) {
+    next(exception)
+  }
+})
+  /*
   const recipe = new Recipe(request.body);
 
   if (!request.token) {
@@ -30,5 +68,6 @@ recipesRouter.post("/", async (request, response) => {
 
   reponse.status(201).json(result);
 });
+*/
 
 module.exports = recipesRouter;
