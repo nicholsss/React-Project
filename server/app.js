3475a8b85f5a65e26a6a09743e25ad75a8f6d13e
Nicholsss <mickyaw1995@gmail.com>
2019-11-19 20:32:24 +0200

    Little fixing

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use('/api/recipes',recipeRouter)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
   .then(() => {
-      console.log('connected to MongoDB')
+      console.log('connected to MongoDB', config.MONGODB_URI)
   })
   .catch((error) => {
       console.log('error connection to MongoDB:', error.message)
